e3c1425dfa62bcbdd57bd3cd5e662a96c84ac2d3
Sidahal <sidahal2018@gmail.com>
2020-12-13 10:34:44 -0500

    terraform01

--- count-avc/main.tf
+++
@@ -1,8 +0,0 @@
-module "instances" {
-  source  = "./ec2"
-  servers = ["web", "db", "app"]
-}
-
-output "Public IPs" {
-  value = "${module.instances.public_ip}"
-}
--- remote-state/main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_s3_bucket" "s3backend" {
-  bucket = "tj-s3-backend-course"
-
-  versioning = {
-    enabled = true
-  }
-
-  force_destroy = true
-}
