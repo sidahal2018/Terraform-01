e965b3ef7915f0a886e32eabc5a1679b618be57d
=siki-sharm <sidahal2018@gmail.com>
2021-03-28 22:57:44 -0400

    updated code

--- PBL/backend.tf
+++ PBL/backend.tf
@@ -1,16 +1,7 @@
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "terraform-state-test-lab"
-  # Enable versioning so we can see the full revision history of our
-  # state files
-  versioning {
-    enabled = true
-  }
-  # Enable server-side encryption by default
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
+terraform {
+    backend "s3" {
+        key = "terraform/tfstate.tfstate"
+        bucket = "siki-remote-backend-2020"
+        region = "us-east-2"
     }
-  }
 }
