3d0f842db08a649736e02970954887a7202865dd
=siki-sharm <sidahal2018@gmail.com>
2021-03-31 15:55:29 -0400

    EFS-Sg add

--- PBL/route_tables.tf
+++ PBL/route_tables.tf
@@ -1,5 +1,5 @@
 
-resource "aws_route" "public" {
+resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
  route {
   cidr_block = "0.0.0.0/0"
--- PBL/security_group.tf
+++ PBL/security_group.tf
@@ -110,3 +110,69 @@ resource "aws_security_group" "webtraffic" {
     Name = var.DB_sg_name
   }
 }
+
+# EFS security Group
+
+
+resource "aws_security_group" "SG" {
+  vpc_id      = aws_vpc.main.id
+  name        = "SG"
+  description = "Allow Inbound Traffic"
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr]
+  }
+
+    ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = "2049"
+    to_port     = "2049"
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr]
+  }
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = [var.vpc_cidr]
+  }
+  
+  egress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags =  {
+    Name = "efs-SG"
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
